9fe196217bda0d86236a35b673b3166d2082579e
shaunblair <shaunbo@gmail.com>
2023-04-18 11:00:06 -0400

    Adding data annotations

--- PatientRecordApi/Models/Patient.cs
+++ PatientRecordApi/Models/Patient.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using PatientRecordApi.Shared;
 
 namespace PatientRecordApi.Models;
@@ -5,8 +6,12 @@ namespace PatientRecordApi.Models;
 public class Patient
 {
     public int Id { get; set; }
+    [Required, MaxLength(50)]
     public string FirstName { get; set; }
+    [Required, MaxLength(75)]
     public string LastName { get; set; }
+    [Required]
     public DateTime Birthday { get; set; }
+    [Required]
     public GenderOption Gender { get; set; }
 }
--- PatientRecordApi/Shared/GenderOptions.cs
+++ PatientRecordApi/Shared/GenderOptions.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PatientRecordApi.Shared;
 
 public class GenderOption
 {
     public int Id { get; set; }
+    [Required, MaxLength(25)]
     public string FullGender { get; set; }
+    [Required]
     public char ShortGender { get; set; }
 }
